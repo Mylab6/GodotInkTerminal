6e683b87f11103b9d7e09202408e6a29f82bb16a
Keith <keith@localhost.localdomain>
2022-07-25 11:54:36 -0400

    o

--- ExampleTerminal.gd
+++ ExampleTerminal.gd
@@ -13,37 +13,37 @@ onready var input := $Input as LineEdit
 
 
 func _ready() -> void:
-    output_print(prompt_template)
-    input.grab_focus()
-    input.connect('gui_input', self, 'on_input')
+	output_print(prompt_template)
+	input.grab_focus()
+	input.connect('gui_input', self, 'on_input')
 
 
 func on_input(event: InputEvent):
-    if event is InputEventKey:
-        if event.is_action_pressed('cmd_enter'):
-            execute_input()
+	if event is InputEventKey:
+		if event.is_action_pressed('cmd_enter'):
+			execute_input()
 
 # Implementing this here as the BashLikeCommands should not allow people
 # to quit a game that uses those commands.
 # warning-ignore:unused_argument
 # warning-ignore:unused_argument
 func cmd_exit(args: Array, stdin: String):
-    get_tree().call_deferred('quit')
-    return ''
+	get_tree().call_deferred('quit')
+	return ''
 
 func execute_input():
-    
-    # Tokenize and execute the input
-    var result := parser.tokenize(input.text)
-    var stdout := parser.execute(result, [self, commands], error_template)
-    
-    # Print everything
-    output_print(input_template % input.text)
-    output_print(stdout)
-    output_print(prompt_template)
-    
-    # Clear the input
-    input.text = ''
+	
+	# Tokenize and execute the input
+	var result := parser.tokenize(input.text)
+	var stdout := parser.execute(result, [self, commands], error_template)
+	
+	# Print everything
+	output_print(input_template % input.text)
+	output_print(stdout)
+	output_print(prompt_template)
+	
+	# Clear the input
+	input.text = ''
 
 func output_print(txt: String):
-    output.bbcode_text += txt
+	output.bbcode_text += txt
